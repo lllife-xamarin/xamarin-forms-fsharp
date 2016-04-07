1e023aa66d45ce7cbe6a1a80493199a1da8f5128
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-04-08 00:41:52 +0700

    Initial commit

---
+++ Hello/Hello.fs
@@ -0,0 +1,24 @@
+﻿namespace Hello
+
+open Xamarin.Forms
+
+type TryFSharpPage() = 
+    inherit ContentPage()
+
+    do base.Content <- Label(Text = "Hello, F# Xamarin.Forms",
+                             HorizontalOptions = LayoutOptions.Center,
+                             VerticalOptions = LayoutOptions.Center,
+                             FontSize = 24.0,
+                             TextColor = Color.Pink,
+                             FontAttributes = FontAttributes.Italic,
+                             Rotation = -15.0)
+
+type App() = 
+    inherit Application()
+    let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
+    let label = Label(HorizontalTextAlignment = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms 2")
+
+    do 
+        stack.Children.Add(label)
+        base.MainPage <-  TryFSharpPage() 
+
---
+++ README.md
@@ -0,0 +1,5 @@
+### Hello XamarinForms in F#
+
+### Reference
+
+- https://github.com/charlespetzold/xamarin-forms-samples
